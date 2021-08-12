4a3a5bd7eacd3036e82d454062a6c8a37e15f4e8
Junxi <tribozheng@gmail.com>
2021-08-13 09:51:51 +1000

    init

---
+++ public/precache-manifest.ffcf846e53924395038565347ad874ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0e638efe16c86a3732bce60abb8f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e0d66182a6dc6c1c608",
+    "url": "/static/css/2.079338fd.chunk.css"
+  },
+  {
+    "revision": "5112a3c275f58bfbc223",
+    "url": "/static/css/main.89472ab1.chunk.css"
+  },
+  {
+    "revision": "4e0d66182a6dc6c1c608",
+    "url": "/static/js/2.cd554f2b.chunk.js"
+  },
+  {
+    "revision": "5112a3c275f58bfbc223",
+    "url": "/static/js/main.52d9e765.chunk.js"
+  },
+  {
+    "revision": "c6bb16cf13a8932a349e",
+    "url": "/static/js/runtime-main.c737abf9.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  }
+]);
